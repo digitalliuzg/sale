32703172e3313f027ddec62f5db1ea8ffe78bfa6
difbit <timo.kekalainen@hotmail.com>
2019-04-25 13:39:32 +0300

    [IMP] Shorter lines and removed fields import

--- sale_order_invoice_commercial_partner/models/sale_order.py
+++ sale_order_invoice_commercial_partner/models/sale_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
@@ -11,8 +11,11 @@ class SaleOrder(models.Model):
     @api.model
     def create(self, vals):
 
-        if vals['team_id'] == self.env.ref('sales_team.salesteam_website_sales').id:
-            vals['partner_invoice_id'] = self.env['res.partner'].browse(vals['partner_invoice_id']).commercial_partner_id.id
+        if vals['team_id'] == \
+        	self.env.ref('sales_team.salesteam_website_sales').id:
+	            vals['partner_invoice_id'] = \
+	            self.env['res.partner'].browse(vals['partner_invoice_id']). \
+	            commercial_partner_id.id
 
         res = super(SaleOrder, self).create(vals)
 
