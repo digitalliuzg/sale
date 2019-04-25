432883d68e11f69a8431319286e1d79434f6c4be
difbit <timo.kekalainen@hotmail.com>
2019-04-25 12:23:53 +0300

    [ADD] A new module that uses commercial_partner_id instead of partner_invoice_id

---
+++ sale_order_invoice_commercial_partner/models/sale_order.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    @api.model
+    def create(self, vals):
+
+        if vals['team_id'] == self.env.ref('sales_team.salesteam_website_sales').id:
+            vals['partner_invoice_id'] = self.env['res.partner'].browse(vals['partner_invoice_id']).commercial_partner_id.id
+
+        res = super(SaleOrder, self).create(vals)
+
+        return res
