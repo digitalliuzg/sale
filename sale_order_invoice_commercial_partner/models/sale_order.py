41a97df1ff5320e05419bd8cc0b9c73453cb49f6
difbit <timo.kekalainen@hotmail.com>
2019-04-25 15:35:56 +0300

    [IMP] Indentation

--- sale_order_invoice_commercial_partner/models/sale_order.py
+++ sale_order_invoice_commercial_partner/models/sale_order.py
@@ -11,13 +11,13 @@ class SaleOrder(models.Model):
     @api.model
     def create(self, vals):
 
-        website_sales_id = self.env.ref('sales_team.salesteam_website_sales').id
+        website_sales_id = self.env.ref(
+            'sales_team.salesteam_website_sales').id
 
         if vals['team_id'] == website_sales_id:
             vals['partner_invoice_id'] = \
                 self.env['res.partner'].browse(
-                    vals['partner_invoice_id'])
-                        .commercial_partner_id.id
+                    vals['partner_invoice_id']).commercial_partner_id.id
 
         res = super(SaleOrder, self).create(vals)
 
