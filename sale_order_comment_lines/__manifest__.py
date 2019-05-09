4f6e483f784c4113df459d30c8b2a55a3988f5c2
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-05-09 13:23:18 +0300

    [ADD] Forced product invoice policy

--- sale_order_comment_lines/__manifest__.py
+++ sale_order_comment_lines/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Allow using comment lines in sale orders',
     'summary': 'Allow using comment lines in sale orders',
     'category': 'Sale',
-    'version': '10.0.1.2.1',
+    'version': '10.0.1.2.2',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
