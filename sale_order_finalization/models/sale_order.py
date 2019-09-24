fb0b4d7c8082125be28456608bc43355542615dd
Jarmo Kortetjärvi <jarmokortetjarvi@users.noreply.github.com>
2019-09-24 14:31:00 +0300

    [ADD] New module for sale order finalization boolean (#27)

---
+++ sale_order_finalization/models/sale_order.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    in_finalization = fields.Boolean(
+        string='In finalization',
+    )
