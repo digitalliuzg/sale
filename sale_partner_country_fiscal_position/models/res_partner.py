dd675a8421d9ff5bd5aa85231d8f29ad6100e5c0
retropikzel <retropikzel@mailbox.org>
2019-11-13 10:06:28 +0200

    [FIX] Fixing indentation according to flake8

--- sale_partner_country_fiscal_position/models/res_partner.py
+++ sale_partner_country_fiscal_position/models/res_partner.py
@@ -9,8 +9,7 @@ class ResPartner(models.Model):
     def create(self, vals_list):
         """Creates a partner with a Fiscal Position based on country_id"""
         for vals in vals_list:
-            if vals.get('country_id') and not \
-                    vals.get('property_account_position_id'):
+            if vals.get('country_id') and not vals.get('property_account_position_id'):
                 account_position_with_vat = self.env['ir.model.data'].search([
                     ('module', '=', 'l10n_fi_liikekirjuri'),
                     ('model', '=', 'account.fiscal.position'),
@@ -30,15 +29,15 @@ class ResPartner(models.Model):
                 ], limit=1)
 
                 if int(vals['country_id']) == self.env['res.country'].search(
-                        [('id', '=', self.env.ref('base.fi').id)]).id:
-                    vals['property_account_position_id'] = \
-                        self.env.ref(account_position_with_vat.complete_name).id
-                elif int(vals['country_id']) in self.env.ref('base.europe')\
-                        .country_ids.ids:
-                    vals['property_account_position_id'] = \
-                        self.env.ref(account_position_y.complete_name).id
+                    [('id', '=', self.env.ref('base.fi').id)]).id:
+                    vals['property_account_position_id'] = self.env.ref(
+                        account_position_with_vat.complete_name).id
+                elif int(vals['country_id']) in self.env.ref(
+                    'base.europe').country_ids.ids:
+                    vals['property_account_position_id'] = self.env.ref(
+                        account_position_y.complete_name).id
                 else:
-                    vals['property_account_position_id'] = \
-                            self.env.ref(account_position_ey.complete_name).id
+                    vals['property_account_position_id'] = self.env.ref(
+                        account_position_ey.complete_name).id
 
         return super(ResPartner, self).create(vals_list)
