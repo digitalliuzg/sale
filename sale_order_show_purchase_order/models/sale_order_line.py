ea8c136115f8824926e16900efd8ff4e0ee26a4e
difbit <36134879+difbit@users.noreply.github.com>
2019-11-15 09:06:56 +0200

    [IMP] Faster PO fetching (#47)

--- sale_order_show_purchase_order/models/sale_order_line.py
+++ sale_order_show_purchase_order/models/sale_order_line.py
@@ -10,29 +10,9 @@ class SaleOrder(models.Model):
 
     @api.multi
     def _get_purchase_ids(self):
-
-        group_list = []
-        purchase_list = []
-
-        purchases = self.purchase_order_ids.search([])
-
-        for record in self.order_line:
-            if record is not False:
-                for line in self.order_line:
-                    if line.procurement_ids.ids != []:
-                        group_list.append(line.procurement_ids.group_id)
-
-            if group_list != []:
-
-                for group in group_list:
-                    for id_number in group.procurement_ids:
-                        if id_number.purchase_id and id_number.purchase_id.ids:
-                            purchase_list.append(id_number.purchase_id.ids[0])
-
-            if purchase_list != []:
-                for i in purchase_list:
-                    self.purchase_order_ids \
-                        = [x for x in purchases.ids if x == i]
+        if self.procurement_group_id:
+            proc_group = self.procurement_group_id.procurement_ids
+            self.purchase_order_ids = proc_group.mapped('purchase_id')
 
     purchase_order_ids = fields.Many2many(
         compute=_get_purchase_ids,
